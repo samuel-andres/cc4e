f4e7ec3613331b011ec7446b4a31a0162158a20d
Samuel Andrés <samuel5848@gmail.com>
2024-03-23 17:42:07 -0300

    Clean repo

--- ch1/hello-input/hello_input.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int usf, euf;
-	printf("Enter US Floor\n");
-	scanf("%d", &usf);
-	euf = usf - 1;
-	printf("EU Floor %d\n", euf);
-}
--- ch1/string-input/string-input.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-  char name[100];
-  printf("Enter name\n");
-  scanf("%1s", name);
-  printf("Hello %s\n", name);
-}
